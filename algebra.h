3e2be9aeb7af45301fea37211525c4edf3c0d642
Verkeia <verkeiap@gmail.com>
2025-11-18 13:38:33 -0500

    created header files and cpp files for the classes

---
+++ algebra.h
@@ -0,0 +1,30 @@
+#ifndef ALGEBRA_H
+#define ALGEBRA_H
+#include <string>
+
+using namespace std;
+
+class Algebra {
+
+public:
+
+
+
+
+private:
+
+
+
+};
+
+
+/*
+. Quadratic equation
+. Slope-intercept/Point Slope form
+. Perimeter
+*/
+
+
+
+
+#endif
