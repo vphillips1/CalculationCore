0a6d71a3ca2b93d332d2d8c4662bd64ca027f997
mphimist <margare@umich.edu>
2025-11-25 14:15:17 -0500

    Add Physics class with various calculation methods

--- physics.h
+++ physics.h
@@ -1,3 +1,51 @@
+#ifndef PHYSICS_H
+#define PHYSICS_H
+#include <string>
+
+using namespace std;
+
+class Physics {
+
+public:
+	// Constructor
+	Physics();
+
+	// Destructor
+	~Physics();
+
+	//Average Velocity
+	double findAverageVelocity(double initPosition, double finalPosition, double initTime, double finalTime);
+	void printAverageVelocityCalc(double initPosition, double finalPosition, double initTime, double finalTime);
+	
+	//Average Speed
+	double findAverageSpeed(double initPosition, double finalPosition, double initTime, double finalTime);
+	void printAverageSpeedCalc(double initPosition, double finalPosition, double initTime, double finalTime);
+
+	//Average Acceleration
+	double findAverageAcceleration(double initVelocity, double finalVelocity, double initTime, double finalTime);
+	void printAverageAccelerationCalc(double initVelocity, double finalVelocity, double initTime, double finalTime);
+
+	// Force Functions
+	double findForce(double mass, double acceleration);	
+	double findMass(double force, double acceleration);
+	double findAcceleration(double force, double mass);
+	void printForceCalc(double force, double mass, double acceleration);
+	void printMassCalc(double force, double mass, double acceleration);
+	void printAccelerationCalc(double force, double mass, double acceleration);
+
+
+
+private:
+	double force;
+	double mass;	
+	double acceleration;
+	double initTime;
+	double finalTime;
+	double initPosition;
+	double finalPosition;
+
+};
+
 /*
 . Average velocity
 . Average speed
@@ -5,3 +53,5 @@
 . Force calculator
 
 */
+
+#endif
