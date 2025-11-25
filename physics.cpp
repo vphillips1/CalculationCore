7f265a49bcbcf863ff28b2e9b47b0db499931297
mphimist <margare@umich.edu>
2025-11-25 14:15:53 -0500

    Implement Physics class with calculations methods

---
+++ physics.cpp
@@ -0,0 +1,126 @@
+#include "physics.h"
+#include <iostream>
+using namespace std;
+
+Physics::Physics()
+{
+	force = 0.0;
+	mass = 0.0;
+	acceleration = 0.0;
+	initPosition = 0.0;
+	finalPosition = 0.0;
+	initTime = 0.0;
+	finalTime = 0.0;
+	finalPosition = 0.0;
+}
+
+Physics::~Physics()
+{
+}
+
+double Physics::findAverageVelocity(double initPosition, double finalPosition, double initTime, double finalTime)
+{
+	if (finalTime - initTime != 0)
+	{
+		return (finalPosition - initPosition) / (finalTime - initTime);
+	}
+	return 0.0;
+}
+
+void Physics::printAverageVelocityCalc(double initPosition, double finalPosition, double initTime, double finalTime)
+{
+	double avgVelocity = findAverageVelocity(initPosition, finalPosition, initTime, finalTime);
+	cout << "Numbers given were: " << endl;
+	cout << "Initial Position: " << initPosition << " m" << endl;
+	cout << "Final Position: " << finalPosition << " m" << endl;
+	cout << "Initial Time: " << initTime << " s" << endl;
+	cout << "Final Time: " << finalTime << " s" << endl;
+	cout << "Calculated Average Velocity: " << avgVelocity << " m/s" << endl;
+}
+
+double Physics::findAverageSpeed(double initPosition, double finalPosition, double initTime, double finalTime)
+{
+	if (finalTime - initTime != 0)
+	{
+		return abs(finalPosition - initPosition) / (finalTime - initTime);
+	}
+	return 0.0;
+}
+
+void Physics::printAverageSpeedCalc(double initPosition, double finalPosition, double initTime, double finalTime)
+{
+	double avgSpeed = findAverageSpeed(initPosition, finalPosition, initTime, finalTime);
+	cout << "Numbers given were: " << endl;
+	cout << "Initial Position: " << initPosition << " m" << endl;
+	cout << "Final Position: " << finalPosition << " m" << endl;
+	cout << "Initial Time: " << initTime << " s" << endl;
+	cout << "Final Time: " << finalTime << " s" << endl;
+	cout << "Calculated Average Speed: " << avgSpeed << " m/s" << endl;
+}
+
+double Physics::findAverageAcceleration(double initVelocity, double finalVelocity, double initTime, double finalTime)
+{
+	if (finalTime - initTime != 0)
+	{
+		return (finalVelocity - initVelocity) / (finalTime - initTime);
+	}
+	return 0.0;
+}
+
+void Physics::printAverageAccelerationCalc(double initVelocity, double finalVelocity, double initTime, double finalTime)
+{
+	double avgAcceleration = findAverageAcceleration(initVelocity, finalVelocity, initTime, finalTime);
+	cout << "Numbers given were: " << endl;
+	cout << "Initial Velocity: " << initVelocity << " m/s" << endl;
+	cout << "Final Velocity: " << finalVelocity << " m/s" << endl;
+	cout << "Initial Time: " << initTime << " s" << endl;
+	cout << "Final Time: " << finalTime << " s" << endl;
+	cout << "Calculated Average Acceleration: " << avgAcceleration << " m/s^2" << endl;
+}
+
+double Physics::findForce(double mass, double acceleration)
+{
+	return mass * acceleration;
+}
+
+double Physics::findMass(double force, double acceleration)
+{
+	if (acceleration != 0)
+	{
+		return force / acceleration;
+	}
+	return 0;
+}
+
+double Physics::findAcceleration(double force, double mass)
+{
+	if (mass != 0)
+	{
+		return force / mass;
+	}	
+	return 0.0;
+}
+
+void Physics::printForceCalc(double force, double mass, double acceleration)
+{
+	cout << "Numbers given were: " << endl;
+	cout << "Mass: " << mass << " kg" << endl;
+	cout << "Acceleration: " << acceleration << " m/s^2" << endl;
+	cout << "Calculated Force: " << force << " N" << endl;
+}
+
+void Physics::printMassCalc(double force, double mass, double acceleration)
+{
+	cout << "Numbers given were: " << endl;
+	cout << "Force: " << force << " N" << endl;
+	cout << "Acceleration: " << acceleration << " m/s^2" << endl;
+	cout << "Calculated Mass: " << mass << " kg" << endl;
+}
+
+void Physics::printAccelerationCalc(double force, double mass, double acceleration)
+{
+	cout << "Numbers given were: " << endl;
+	cout << "Force: " << force << " N" << endl;
+	cout << "Mass: " << mass << " kg" << endl;
+	cout << "Calculated Acceleration: " << acceleration << " m/s^2" << endl;
+}
